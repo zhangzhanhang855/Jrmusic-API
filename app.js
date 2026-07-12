2cb1aa9dab99edb8ff971fdc607ad6624f4967e7
zhangzhanhang855 <zhangzhanhang855@gmail.com>
2026-07-12 11:12:07 +0800

    Refactor to use serve instead of startCmd

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const { startCmd } = require('NeteaseCloudMusicApi')
+const { serve } = require('NeteaseCloudMusicApi')
 
-// 启动网易云音乐 API 服务，Render 会自动分配端口
-startCmd({
+// 启动网易云音乐 API 服务
+serve({
   port: process.env.PORT || 3000
+}).then(() => {
+  console.log('JRmusic 后端 API 服务已成功启动！')
 })
