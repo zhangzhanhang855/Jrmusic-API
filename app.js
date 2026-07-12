11399fe36579b416ed6c2eb98be598f8de9c6f87
zhangzhanhang855 <zhangzhanhang855@gmail.com>
2026-07-12 11:13:49 +0800

    Simplify server startup by importing directly
    Replaced custom server start with direct server import.

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-const { serve } = require('NeteaseCloudMusicApi')
-
-// 启动网易云音乐 API 服务
-serve({
-  port: process.env.PORT || 3000
-}).then(() => {
-  console.log('JRmusic 后端 API 服务已成功启动！')
-})
+// 直接加载网易云音乐 API 内部已经封装好的服务器入口
+require('NeteaseCloudMusicApi/server')
